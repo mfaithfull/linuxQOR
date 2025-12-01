151280c7eeb3fdb46b8dbba439d7f03957ca7c0a
Matthew Faithfull <mfaithfull@btopenworld.com>
2025-12-01 14:32:06 +0000

    Still attempting to clean up Windows build. Link dependencies missing despite repeatedly adding the same.

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -2,7 +2,7 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++2a -fcoroutines -fno-threadsafe-statics -fvisibility=hidden")
 endif()
 if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc /GR")
 endif()
 
 set(global_defines -Dqor_pp_unicode)
