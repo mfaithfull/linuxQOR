ae3feab4f98d4b3639af212e6f03d07a574eb08e
Matthew Faithfull <mfaithfull@btopenworld.com>
2026-06-10 23:46:28 +0100

    GCC build flag change for debugging concept requirements

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -1,5 +1,5 @@
 if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++2a -fcoroutines -fno-threadsafe-statics -fvisibility=hidden -Wno-attributes")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++2a -fcoroutines -fno-threadsafe-statics -fvisibility=hidden -fconcepts-diagnostics-depth=3 -Wno-attributes")
 endif()
 if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc /GR")
